58baeaed950d075602aed4814614229791756f4e
mlapinm <mlapin@rambler.ru>
2020-11-27 09:40:57 +0300

    b1231progressbar

---
+++ b1231progress/app/src/main/java/com/example/b1231progress/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.b1231progress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        textView = (TextView)findViewById(R.id.textView);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus<100){
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus + "/" + progressBar.getMax());
+                        }
+                    });
+                    try {
+                        Thread.sleep(200);
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }).start();
+
+    }
+}
