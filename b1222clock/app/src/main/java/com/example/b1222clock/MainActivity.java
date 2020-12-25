2d1178e87388b61ca9217d76f2eae787c150c2d2
Mikhail Lapin <mlapin@rambler.ru>
2020-12-25 16:57:57 +0300

    42

--- b1222clock/app/src/main/java/com/example/b1222clock/MainActivity.java
+++ b1222clock/app/src/main/java/com/example/b1222clock/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.b1222clock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Keep the device awake
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+//        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
--- c2022clock/app/src/main/java/com/example/freon/c2022clock/MainActivity.java
+++ c2022clock/app/src/main/java/com/example/freon/c2022clock/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.b1224battery;
+package com.example.freon.c2022clock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,13 +6,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
 import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView1;
+    TextView textView1;
+    int k = 0;
 
     private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -22,10 +25,21 @@ public class MainActivity extends AppCompatActivity {
             int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
             boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
                     status == BatteryManager.BATTERY_STATUS_FULL;
+            k += 1;
             String s1 = "";
             s1 += String.valueOf(level) + " %" + " ";
-            s1 += "isCharging : " + isCharging + "\n";
+            s1 += "isCharging : " + isCharging + " ";
+            s1 += k;
             textView1.setText(s1);
+            if(isCharging){
+                textView1.setTextColor(Color.YELLOW);
+                //Keep the device awake
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }else {
+                textView1.setTextColor(Color.CYAN);
+                //Keep the device awake
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
         }
     };
 
@@ -35,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView1 = (TextView)findViewById(R.id.textView1);
-        this.registerReceiver(this.mBroadcastReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+        this.registerReceiver(this.mBroadcastReceiver,
+                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+
+
     }
 }
