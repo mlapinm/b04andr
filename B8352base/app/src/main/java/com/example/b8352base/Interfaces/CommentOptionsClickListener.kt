096c05e09bd24516f4e2038ea719699d14f4fdd4
mlapinm <mlapin@rambler.ru>
2021-02-23 19:24:33 +0300

    B8352base empty

---
+++ B8352base/app/src/main/java/com/example/b8352base/Interfaces/CommentOptionsClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.b8352base.Interfaces
+
+import com.example.b8352base.Model.Comment
+
+interface CommentOptionsClickListener {
+    fun optionsMenuClicked( comment : Comment )
+}
---
+++ B8352base/app/src/main/java/com/example/b8352base/Interfaces/ThoughtOptionsClickListener.kt
@@ -0,0 +1,8 @@
+package com.example.b8352base.Interfaces
+
+import com.example.b8352base.Model.Thought
+
+interface ThoughtOptionsClickListener {
+    fun thoughtOptionMenuClicked(thought: Thought)
+
+}
